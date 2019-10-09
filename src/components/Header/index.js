836cd52dc319a4cbcbcf821056acfd116bdeb7d1
Thiago Marinho <tgmarinho@gmail.com>
2019-10-09 15:56:40 -0400

    GoBarber Web - Aula 20 - Configurando o Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo-purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Thiago Marinho</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard
+"
+              alt="Thiago Marinho"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
+import Header from '~/components/Header';
 
 export default function DefaultLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Header>{children}</Header>
+    </Wrapper>
+  );
 }
 
 DefaultLayout.propTypes = {
