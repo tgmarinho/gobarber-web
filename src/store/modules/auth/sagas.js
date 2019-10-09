3f9c42122f59a2459b0fb81e4e050fc34c20ba38
Thiago Marinho <tgmarinho@gmail.com>
2019-10-09 12:22:13 -0400

    GoBarber Web - Aula 19 - Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -19,6 +19,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Baerer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -46,7 +48,16 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+  const { token } = payload.auth;
+  if (token) {
+    api.defaults.headers.Authorization = `Baerer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
